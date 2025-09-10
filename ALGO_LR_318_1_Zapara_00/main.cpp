147032bb176f215132c14d6a006ba09af34f4dec
glebzapara02 <h.i.zapara@student.khai.edu>
2025-09-10 10:39:07 +0300

    Add files via upload

---
+++ ALGO_LR_318_1_Zapara_00/main.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    //задача 1
+    
+    double a;
+    cout << "a = ";
+    cin >> a;
+    
+    double P = 4 * a;
+    cout << "Периметр = " << P << endl;
+    
+    return 0;
+}
